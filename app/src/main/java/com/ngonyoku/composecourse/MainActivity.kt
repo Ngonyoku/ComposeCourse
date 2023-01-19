3353877ca9612bcba5625f5dbbe2ac63b8d59458
NGONYOKU <ngonyoku001@gmail.com>
2023-01-19 18:32:50 +0300

    Understood

--- app/src/main/java/com/ngonyoku/composecourse/MainActivity.kt
+++ app/src/main/java/com/ngonyoku/composecourse/MainActivity.kt
@@ -3,19 +3,20 @@ package com.ngonyoku.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.ngonyoku.composecourse.ui.theme.ComposeCourseTheme
 
@@ -24,33 +25,58 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Greetings(name = "NGONYOKU")
+            LearningColumns()
         }
     }
 }
 
-@Composable //Views are Now composables
-fun Greetings(name: String) {
-    Surface(
+@Composable
+fun LearningRows() {
+    Row(
         modifier = Modifier
-            .fillMaxSize()
+            .background(Color.LightGray)
+            .fillMaxSize(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(
-            text = "Hello $name",
-            fontSize = 20.sp,
-            fontStyle = FontStyle.Italic,
-            textAlign = TextAlign.Center,
-            color = Color.Magenta,
-        )
+        for (i in 1..5) {
+            Text(
+                text = "Value $i",
+                fontSize = 20.sp,
+                fontStyle = FontStyle.Normal,
+                fontFamily = FontFamily.Monospace
+            )
+        }
+    }
+}
+
+@Composable
+fun LearningColumns() {
+    /* The Column Trailing Lambda is a used to stack views on top of each other
+    * */
+    Column(
+        modifier = Modifier
+            .background(Color.LightGray)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceEvenly
+    ) {
+        for (i in 1..10) {
+            Text(
+                text = "Value $i",
+                fontSize = 20.sp,
+                fontStyle = FontStyle.Italic,
+                fontFamily = FontFamily.Monospace
+            )
+        }
     }
 }
 
 @Composable
 @Preview(showBackground = true)
-fun Preview() {
-    Surface(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Greetings(name = "RickMan!")
+fun displayPreview() {
+    Surface {
+        LearningColumns()
+//        LearningRows()
     }
 }
