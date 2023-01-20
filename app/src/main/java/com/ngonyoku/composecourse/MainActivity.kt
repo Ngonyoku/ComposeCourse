93dfecf3212958e5b134470eec985f2ca2db292d
NGONYOKU <ngonyoku001@gmail.com>
2023-01-20 19:39:01 +0300

    Understood

--- app/src/main/java/com/ngonyoku/composecourse/MainActivity.kt
+++ app/src/main/java/com/ngonyoku/composecourse/MainActivity.kt
@@ -3,6 +3,8 @@ package com.ngonyoku.composecourse
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -13,82 +15,62 @@ import androidx.compose.foundation.verticalScroll
 
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 
 import androidx.compose.ui.text.style.TextAlign
 
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        /**
-         * LazyColumn is the equivalent of RecyclerView
-         * */
-
         setContent {
-//            LazyColumnImplementationUsingItemsIndexed(listOf("Java", "Kotlin", "C++", "Python"))
-//            LazyColumnImplementationUsingItems()
-            ScrollableColumn()
+            Column(Modifier.fillMaxSize()) {
+
+                val color = remember {
+                    mutableStateOf(Color.Cyan)
+                }
+
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                ) {
+                    color.value = it
+                }
+
+                Box(
+                    modifier = Modifier
+                        .background(color.value)
+                        .weight(1f)
+                        .fillMaxSize()
+                )
+            }
         }
     }
 }
 
 @Composable
-fun ScrollableColumn() {
-    val scrollState = rememberScrollState()
-    Column(
-        Modifier
-            .verticalScroll(scrollState)
-    ) {
-        for (i in 1..100) {
-            Text(
-                text = "Item at position -> $i",
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 50.dp),
-                textAlign = TextAlign.Center,
-                fontSize = 20.sp
-            )
-        }
-    }
+fun ColorBox(modifier: Modifier = Modifier, updateColor: (Color) -> Unit) {
+    Box(
+        modifier = modifier
+            .background(Color.Blue)
+            .clickable {
+                updateColor(
+                    Color(
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        1f
+                    )
+                )
+            }
+    )
 }
-
-@Composable
-fun LazyColumnImplementationUsingItemsIndexed(list: List<String>) {
-    LazyColumn() {
-        itemsIndexed(
-            list
-        ) { index, item ->
-
-            Text(
-                text = item,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 50.dp),
-                textAlign = TextAlign.Center,
-                fontSize = 20.sp
-            )
-        }
-    }
-}
-
-@Composable
-fun LazyColumnImplementationUsingItems() {
-    LazyColumn() {
-        items(20) {
-            Text(
-                text = "Item Index -> $it", //Displays the Index
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 50.dp),
-                textAlign = TextAlign.Center,
-                fontSize = 20.sp
-            )
-        }
-    }
-}
-
